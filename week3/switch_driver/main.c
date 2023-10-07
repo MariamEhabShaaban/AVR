75cf9066356bab81bf9311b1ea4634c50bc0be91
Mariamehab2003 <99998896+Mariamehab2003@users.noreply.github.com>
2023-10-07 08:52:29 +0300

    Add files via upload

---
+++ week3/switch_driver/main.c
@@ -0,0 +1,28 @@
+#include "stdTypes.h"
+#include "errorStates.h"
+#include "DIO_int.h"
+#include <util/delay.h>
+int main(void){
+	SWITCH_enuint();
+		 LED_enuint();
+
+		u8 switch_stat=20;
+		while(1){
+
+			ES_t Status= SWITCH_STATUS(D_u8PORTB ,D_u8PIN0 , &switch_stat);
+			if(Status==ES_OK){
+
+				if(switch_stat==0){
+
+				PIN_LED_ON(D_u8PORTA ,D_u8PIN0 );
+			}
+			   else{
+				PIN_LED_OFF(D_u8PORTA ,D_u8PIN0 );
+			}
+			}
+
+}
+
+	return 0;
+}
+
